e4532e3fb06417a00256b48b9981f9173ab3ab71
lipeiwen <lipeiwen@chaintor.com>
2021-01-26 16:42:24 +0800

    Updates

---
+++ precache-manifest.59f4a4aceadb935324178c0c5782243a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67484789b4e27375c6055fbe7de6d433",
+    "url": "/react-mobx-apple-app/index.html"
+  },
+  {
+    "revision": "78cda222c3b1343f552c",
+    "url": "/react-mobx-apple-app/static/css/main.77997688.chunk.css"
+  },
+  {
+    "revision": "a88e6075c03c035650d4",
+    "url": "/react-mobx-apple-app/static/js/2.c6ee563d.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/react-mobx-apple-app/static/js/2.c6ee563d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78cda222c3b1343f552c",
+    "url": "/react-mobx-apple-app/static/js/main.bfc668a6.chunk.js"
+  },
+  {
+    "revision": "e7ef77f17025244dcda6",
+    "url": "/react-mobx-apple-app/static/js/runtime-main.7c3f11d1.js"
+  }
+]);
